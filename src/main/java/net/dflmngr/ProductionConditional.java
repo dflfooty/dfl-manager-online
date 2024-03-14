a0ed96ce68da9659848c1f5de6acfc12b2dd257a
Keith Lockwood <kwlockwo@gmail.com>
2024-03-14 12:16:14 +1100

    fix results page when no data

--- src/main/java/net/dflmngr/ProductionConditional.java
+++ src/main/java/net/dflmngr/ProductionConditional.java
@@ -3,11 +3,12 @@ package net.dflmngr;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.lang.NonNull;
 
 public class ProductionConditional implements Condition {
 
 	@Override
-	public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+	public boolean matches(@NonNull ConditionContext context, @NonNull AnnotatedTypeMetadata metadata) {
 		String appEnv = System.getenv("APP_ENV");
 		return (appEnv != null && appEnv.equalsIgnoreCase("production"));
 	}
--- src/main/java/net/dflmngr/WebConfiguration.java
+++ src/main/java/net/dflmngr/WebConfiguration.java
@@ -3,6 +3,7 @@ package net.dflmngr;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,7 +21,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     }
 	
 	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 }
